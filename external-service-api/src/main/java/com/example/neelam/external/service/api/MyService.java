4b4c2f909ffa24d6fc901a2a701b07f75dff14bc
NeelamLakra <neelam@knoldus.in>
2018-06-20 11:41:09 +0530

    external service data persisted in cassandra

--- external-service-api/src/main/java/com/example/neelam/external/service/api/MyService.java
+++ external-service-api/src/main/java/com/example/neelam/external/service/api/MyService.java
@@ -10,11 +10,14 @@ import static com.lightbend.lagom.javadsl.api.Service.named;
 
 public interface MyService extends Service {
     ServiceCall<NotUsed, ExternalJson> invokeExternal();
+    ServiceCall<NotUsed,String> postExternalData();
     @Override
     default Descriptor descriptor() {
         return named("my-service")
                 .withCalls(
-                        Service.restCall(Method.GET,"/api/invokeExternal",this::invokeExternal)
+                        Service.restCall(Method.GET,"/api/invokeExternal",this::invokeExternal),
+                        Service.restCall(Method.POST,"/api/postExternalJson",this::postExternalData)
+
                 ).withAutoAcl(true);
     }
 }
--- external-service-impl/src/main/java/com/example/neelam/external/service/impl/MyServiceImpl.java
+++ external-service-impl/src/main/java/com/example/neelam/external/service/impl/MyServiceImpl.java
@@ -5,19 +5,31 @@ import com.example.neelam.external.service.api.ExternalJson;
 import com.example.neelam.external.service.api.ExternalService;
 import com.example.neelam.external.service.api.MyService;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
+import com.lightbend.lagom.javadsl.persistence.cassandra.CassandraSession;
 
 import javax.inject.Inject;
 
 public class MyServiceImpl implements MyService {
-    ExternalService externalService;
+    private final ExternalService externalService;
+    private final CassandraSession session;
     @Inject
-    public MyServiceImpl(ExternalService externalService) {
-    this.externalService=externalService;
+    public MyServiceImpl(ExternalService externalService, CassandraSession session) {
+    
+        this.externalService=externalService;
+        this.session=session;
     }
 
     @Override
     public ServiceCall<NotUsed, ExternalJson> invokeExternal() {
         return req->externalService.postStudent().invoke().thenApply(row->row);
+        }
+        
+        @Override
+    public ServiceCall<NotUsed, String> postExternalData(){
+        ExternalJson externalJson = externalService.postStudent().invoke().thenApply(row -> row).toCompletableFuture().join();
+        return req->session.executeWrite("insert into user.external_json(userId,id,title,body) values(?,?,?,?)",externalJson.getId(),
+                                        externalJson.getUserId(),externalJson.getTitle(),externalJson.getBody()).thenApply(done ->"inserted");
+        
         }
     }
 
